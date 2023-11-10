96f074aa84905a15fb206e079babb2243f87fa3b
Lucas <85856491+Xugnon@users.noreply.github.com>
2023-11-09 23:55:49 -0300

    feat: add response body on documentation of /me route

--- api/src/modules/users/users.controller.ts
+++ api/src/modules/users/users.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 
 import { UsersService } from './users.service';
 import { ActiveUserId } from 'src/shared/decorators/ActiveUserId';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOkResponse } from '@nestjs/swagger';
 
 @ApiBearerAuth('accessToken')
 @Controller('users')
@@ -10,6 +10,14 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('/me')
+  @ApiOkResponse({
+    schema: {
+      example: {
+        name: 'John',
+        email: 'mail@mail.com',
+      },
+    },
+  })
   me(@ActiveUserId() userId: string) {
     return this.usersService.getUserById(userId);
   }
